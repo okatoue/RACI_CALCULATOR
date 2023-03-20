47ff8600255086c2bfa0ff138a9eb198098fb8b1
okatoue <101699984+okatoue@users.noreply.github.com>
2023-03-20 12:25:05 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from "react";
+import {DropDown} from "./DropDown";
+
+const App = () => {
+
+  return (
+      <div>
+        <DropDown />
+      </div>
+  );
+};
+
+export default App;
